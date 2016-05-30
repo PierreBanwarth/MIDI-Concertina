78517c0b52201cb89c65b4fd63b021edc1c36ea1
Banwarth Pierre <pi.banwarth@gmail.com>
2016-05-31 01:04:22 +0200

    Adding comments on arduino code
    Adding comments

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,10 @@ void setup()
 {
   MIDI.begin(1);
   Serial.begin(115200);
-
+  
+  // Hairlesse serial baudrate Set 
+  // now we need to setup each input Pins
+  
   pinMode(2,INPUT);
   digitalWrite(2,HIGH);
   pinMode(3,INPUT);
@@ -51,17 +54,22 @@ void setup()
   //  Set MIDI baud rate:
 
 }
+// need to read if a button was ever pressed or not 
 uint8_t oldStatePousser[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 uint8_t oldStateTirer[] =   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 void note(uint8_t bouton, uint8_t sens_soufflet, uint8_t noteA, uint8_t noteB, uint8_t index, uint8_t octave){//, uint8_t oldStatePousser, uint8_t oldStateTirer
-  uint8_t velocity = 100;//velocity of MIDI notes, must be between 0 and 127
-  //higher velocity usually makes MIDI instruments louder
+  uint8_t velocity = 100;
+  //velocity of MIDI notes, must be between 0 and 127
+ // maybe i need to change the velocity value.
+ //higher velocity usually makes MIDI instruments louder
   if ( bouton == LOW ) {
-    if(sens_soufflet == LOW ){
-      // on pousse sur le bouton et sur le soufflet
+    if(sens_soufflet == LOW ){// we are pushing
       if(oldStatePousser[index] == 0){
+        // the note was not pushed before
+        // so play A note and save state
         MIDI.sendNoteOn(noteA + octave ,velocity,1);
         oldStatePousser[index] = 1;
+        // this part is needed in case of air flow change stop the BoteA and edit old state
         if( oldStateTirer[index] == 1){
           MIDI.sendNoteOff(noteB + octave ,0,1);
           oldStateTirer[index] = 0;
@@ -69,10 +77,13 @@ void note(uint8_t bouton, uint8_t sens_soufflet, uint8_t noteA, uint8_t noteB, u
       }
     }
     else
-    {// on tire sur le soufflet et on appuie sur le bouton
+    {// we pull
       if(oldStateTirer[index] == 0){
+        // and the note was not pulled before
+        // so we play noteB and save state.
         MIDI.sendNoteOn(noteB + octave,velocity,1);
         oldStateTirer[index] = 1;
+        //This part is need in case of air flow change to stop the old NoteA and edit old state
         if(oldStatePousser[index] == 1){
           MIDI.sendNoteOff(noteA + octave,0,1);
           oldStatePousser[index] = 0;
@@ -83,21 +94,22 @@ void note(uint8_t bouton, uint8_t sens_soufflet, uint8_t noteA, uint8_t noteB, u
   else
   {
     if(oldStatePousser[index] == 1){
+      // So we cut off the note and we set the old position to off
       MIDI.sendNoteOff(noteA + octave,0,1);
       oldStatePousser[index] = 0;
     }if( oldStateTirer[index] == 1){
-      // on appuie pas sur le bouton ma
+      // So we cut off the note and we set the old position to off
       MIDI.sendNoteOff( noteB + octave,0,1);
       oldStateTirer[index] = 0;
     }
   }
 }
-//main droite
+// Right hand note config
 //
 // {23,26,31,25,38,12,19,24,28,31,16,21,25,33,32}
 // {26,30,33,36,40,19,23,26,29,33,17,22,27,31,34}
 //
-// main gauche
+//  Left hand note config
 //
 // {43, 47, 50, 55, 59,  36, 40, 43, 48, 52, 37, 45, 44, 49, 53 }
 // {42, 45, 48, 52, 54,  35, 38, 41, 45, 47, 39, 43, 46, 51, 57 }
@@ -105,7 +117,7 @@ void loop()
 {
   int  etatButton[]= {digitalRead(2),digitalRead(3), digitalRead(4),digitalRead(5),digitalRead(6), digitalRead(7),digitalRead(8), digitalRead(9), digitalRead(10), digitalRead(11), digitalRead(12), digitalRead(A0), digitalRead(A1), digitalRead(A2), digitalRead(A3)};
   //int etatButton[]= {digitalRead(A0), digitalRead(A1), digitalRead(A2),digitalRead(A3), digitalRead(A4),digitalRead(12),digitalRead(13), digitalRead(2),digitalRead(3), digitalRead(4), digitalRead(5), digitalRead(6), digitalRead(7), digitalRead(8), digitalRead(9)};
-
+  
   uint8_t pousser[] ={43, 47, 50, 55, 59,  36, 40, 43, 48, 52, 37, 45, 44, 49, 53 };
   uint8_t tirer[]   = {42, 45, 48, 52, 54,  35, 38, 41, 45, 47, 39, 43, 46, 51, 57 };
   uint8_t poussertirer = digitalRead(A4);
